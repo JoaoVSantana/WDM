8f7e8d63985ae88ef66527dbe6f6d0e9764f37fa
JoaoVSantana <joao10victorsantana@gmail.com>
2024-08-13 18:40:36 -0300

    Anotações removidas

--- wdm/src/main/java/net.weg.wdm/entity/Dispositivo.java
+++ wdm/src/main/java/net.weg.wdm/entity/Dispositivo.java
@@ -25,10 +25,6 @@ public class Dispositivo {
     private String marca;
     @Column(nullable = false, length = 50)
     private String modelo;
-    //mappedBy serve para classes que possuem "ligações duplas" onde ambas se referenciam
-    //Colocando mappedBy na classe que for considerada Many, o sistema faz apenas uma ligação
-    /* Com o mappedBy, quando "dispositivo" for registrado ele usa o próprio Id para procurar
-    na tabela do banco e se encontrar algo que é seu, ele registra na sua classe */
     @OneToMany(mappedBy = "dispositivo")
     @JsonIgnore
     @ToString.Exclude
